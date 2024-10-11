c93da66e2540e72108b5d4e08e43c303ae8f0075
Pythonic2 <igoormarinhosilva@gmail.com>
2024-10-11 13:33:36 -0300

    corrigindo exibir erro lgin

--- authentication/views.py
+++ authentication/views.py
@@ -3,6 +3,16 @@ from django.views.generic import CreateView
 from django.contrib.auth.views import LoginView
 from .forms import SiginUpForm, LoginForm
 from django.contrib.auth import authenticate, login, get_user_model
+from django.contrib.auth.forms import AuthenticationForm
+from django.utils.translation import gettext_lazy
+from .models import CustomUser
+from django.contrib.auth import logout
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
 
 class RegisterView(CreateView):
     template_name = 'accounts/register.html'
@@ -54,3 +64,20 @@ class LoginView(LoginView):
         context['title']='Login'
 
         return render(request, self.template_name, context)
+    
+
+    def form_invalid(self, form: AuthenticationForm):
+        username = form.cleaned_data.get('username')
+        user_existis = CustomUser.objects.filter(username=username)
+        error_messages = {
+            'invalid_login':gettext_lazy('verifique o usuario e senha e tente novamente'),
+            'inactive':gettext_lazy('Usuario Inativo'),
+        }
+        response = super().form_invalid(form)
+
+        if not user_existis:
+            response['erro'] = error_messages['invalid_login']
+        else:
+            response['erro'] = error_messages['inactive']
+        
+        return response
